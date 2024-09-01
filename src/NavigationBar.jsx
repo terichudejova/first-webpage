914c0c47aa2b823f1cd9ecb76976918542a33061
Teri Chudějová <tereza.chudejova@gmail.com>
2024-09-01 11:14:49 +0200

    fix hamburger menu so it closes when you click on something on the menu or anywhere on the web

--- src/NavigationBar.jsx
+++ src/NavigationBar.jsx
@@ -1,13 +1,31 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import "./NavigationBar.css"
 
 export default function NavigationBar() {
     const [isOpen, setIsOpen] = useState(false);
+    const navRef = useRef(null);
   
     const handleToggle = () => {
       setIsOpen(!isOpen);
     };
+
+    const handleCloseMenu = () => {
+      setIsOpen(false);
+    }
+
+    useEffect(() => {
+      const handleClickOutside = (event) => {
+          if (navRef.current && !navRef.current.contains(event.target)) {
+              setIsOpen(false);
+          }
+      };
+
+      document.addEventListener('mousedown', handleClickOutside);
+      return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+      };
+    }, []);
   
     return (
       <div className="hamburger-menu">
@@ -16,10 +34,10 @@ export default function NavigationBar() {
           <div className="bar"></div>
           <div className="bar"></div>
         </div>
-        <nav className={`nav-menu ${isOpen ? 'open' : ''}`}>
-          <a href="#landingPage">O nás</a>
-          <a href="#nadpis">Nabídka</a>
-          <a href="#kontakt">Kontakt</a>
+        <nav ref={navRef} className={`nav-menu ${isOpen ? 'open' : ''}`}>
+          <a href="#landingPage" onClick={handleCloseMenu}>O nás</a>
+          <a href="#nadpis" onClick={handleCloseMenu}>Nabídka</a>
+          <a href="#kontakt" onClick={handleCloseMenu}>Kontakt</a>
         </nav>
       </div>
     );
